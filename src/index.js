85fd58a8303081ac812f4e0179ff1fc55770c71f
membae <membaphilip1@gmail.com>
2024-07-19 21:36:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const newelement=document.getElementById("new-task-description").value;
+    const list=document.createElement("li");
+
+    list.textContent=newelement;
+    taskList.appendChild(list)
+    form.reset();
+  })
 });
